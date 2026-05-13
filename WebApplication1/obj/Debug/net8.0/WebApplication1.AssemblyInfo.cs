12a8740d64296b8e817c1bc44a1e56a95fd86544
Tupikin Dmitrii Evgenyevich <detupikin@edu.hse.ru>
2026-05-13 13:55:50 +0300

    last

--- WebApplication1/obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
+++ WebApplication1/obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+32118f307cd3c861f8b478964fc63b369318ffc6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e3e530bec9cfb66ba3286e4f06383a278efb2ca4")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
